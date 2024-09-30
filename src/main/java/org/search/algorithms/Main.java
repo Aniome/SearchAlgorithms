0fd4d1883b4d4b164e9d9768d3161ac89c865c81
user <vaganov.kostya2000@yandex.ru>
2024-09-30 16:10:00 +0500

    add Linear search

--- src/main/java/org/search/algorithms/Main.java
+++ src/main/java/org/search/algorithms/Main.java
@@ -1,7 +1,14 @@
 package org.search.algorithms;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Linear search with barrier
+        //Binary search
+        //Interpolation search
+        SearchAlgorithms searchAlgorithms = new SearchAlgorithms(50);
+        System.out.println(searchAlgorithms.sequentialSearch(searchAlgorithms.getFirstValue()));
+        System.out.println(Arrays.toString(searchAlgorithms.getArray()));
     }
 }
--- src/main/java/org/search/algorithms/SearchAlgorithms.java
+++ src/main/java/org/search/algorithms/SearchAlgorithms.java
@@ -1,4 +1,31 @@
 package org.search.algorithms;
 
 public class SearchAlgorithms {
+    private final int[] array;
+    private final int length;
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public int getFirstValue() {
+        return array[0];
+    }
+
+    public SearchAlgorithms(int size) {
+        array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+        length = size;
+    }
+
+    public boolean sequentialSearch(int k) {
+        for (int i = 0; i < length; i++)
+        {
+            if (array[i] == k)
+                return true;
+        }
+        return false;
+    }
 }
